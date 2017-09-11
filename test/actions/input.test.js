7bc569eac745b97137e1db8b9271493b3e5c8a20
Shin'ya Ueoka <ueokande@i-beam.org>
2017-09-11 21:37:51 +0900

    fix input actions test

--- test/actions/input.test.js
+++ test/actions/input.test.js
@@ -4,14 +4,18 @@ import * as inputActions from '../../src/actions/input';
 
 describe("input actions", () => {
   describe("keyPress", () => {
-    let action = inputActions.keyPress(123, true);
-    expect(action.type).to.equal(actions.INPUT_KEY_PRESS);
-    expect(action.code).to.equal(123);
-    expect(action.ctrl).to.be.true;
+    it('create INPUT_KEY_PRESS action', () => {
+      let action = inputActions.keyPress(123, true);
+      expect(action.type).to.equal(actions.INPUT_KEY_PRESS);
+      expect(action.code).to.equal(123);
+      expect(action.ctrl).to.be.true;
+    });
   });
 
   describe("clearKeys", () => {
-    let action = inputActions.clearKeys();
-    expect(action.type).to.equal(actions.INPUT_CLEAR_KEYS);
+    it('create INPUT_CLEAR_KEYSaction', () => {
+      let action = inputActions.clearKeys();
+      expect(action.type).to.equal(actions.INPUT_CLEAR_KEYS);
+    });
   });
 });
