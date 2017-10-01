8c9051076f7782b898ab2f848b9afe4a694c3065
Shin'ya Ueoka <ueokande@i-beam.org>
2017-10-01 12:15:35 +0900

    use validation on settings

--- src/components/setting.js
+++ src/components/setting.js
@@ -1,4 +1,5 @@
 import * as settingActions from '../actions/setting';
+import { validate } from '../shared/validators/setting';
 
 export default class SettingComponent {
   constructor(wrapper, store) {
@@ -9,6 +10,9 @@ export default class SettingComponent {
     let form = doc.getElementById('vimvixen-settings-form');
     form.addEventListener('submit', this.onSubmit.bind(this));
 
+    let plainJson = form.elements['plain-json'];
+    plainJson.addEventListener('input', this.onPlainJsonChanged.bind(this));
+
     store.dispatch(settingActions.load());
   }
 
@@ -18,6 +22,16 @@ export default class SettingComponent {
     };
     this.store.dispatch(settingActions.save(settings));
     e.preventDefault();
+  }
+
+  onPlainJsonChanged(e) {
+    try {
+      let settings = JSON.parse(e.target.value);
+      validate(settings);
+      e.target.setCustomValidity('');
+    } catch (err) {
+      e.target.setCustomValidity(err.message);
+    }
   }
 
   update() {
