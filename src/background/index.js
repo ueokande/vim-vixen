a74a8b537e8a82f1af63667fd73869b83d8b7d0d
Shin'ya Ueoka <ueokande@i-beam.org>
2017-10-01 17:17:20 +0900

    BackgroundInputComponent

--- src/background/index.js
+++ src/background/index.js
@@ -1,15 +1,10 @@
-import * as keys from './keys';
-import * as inputActions from '../actions/input';
-import * as operationActions from '../actions/operation';
 import * as consoleActions from '../actions/console';
 import * as settingsActions from '../actions/setting';
 import BackgroundComponent from '../components/background';
+import BackgroundInputComponent from '../components/background-input';
 import reducers from '../reducers';
 import messages from '../content/messages';
 import * as store from '../store';
-
-let prevInput = [];
-let settings = {};
 
 const backgroundStore = store.createStore(reducers, (e, sender) => {
   console.error('Vim-Vixen:', e);
@@ -18,22 +13,10 @@ const backgroundStore = store.createStore(reducers, (e, sender) => {
   }
 });
 const backgroundComponent = new BackgroundComponent(backgroundStore);
+const backgroundInputComponent = new BackgroundInputComponent(backgroundStore);
 backgroundStore.subscribe((sender) => {
   backgroundComponent.update(sender);
-});
-backgroundStore.subscribe((sender) => {
-  let currentInput = backgroundStore.getState().input;
-  if (JSON.stringify(prevInput) === JSON.stringify(currentInput)) {
-    return;
-  }
-  prevInput = currentInput;
-
-  if (currentInput.keys.length === 0) {
-    return;
-  }
-  if (sender) {
-    return keyQueueChanged(backgroundStore.getState(), sender);
-  }
+  backgroundInputComponent.update(sender);
 });
 backgroundStore.subscribe((sender) => {
   if (sender) {
@@ -43,34 +26,5 @@ backgroundStore.subscribe((sender) => {
     });
   }
 });
-backgroundStore.subscribe(() => {
-  let state = backgroundStore.getState().setting;
-  if (!state.settings.json) {
-    return;
-  }
-  settings = JSON.parse(backgroundStore.getState().setting.settings.json);
-});
 
-const keyQueueChanged = (state, sender) => {
-  let prefix = keys.asKeymapChars(state.input.keys);
-  let matched = Object.keys(settings.keymaps).filter((keyStr) => {
-    return keyStr.startsWith(prefix);
-  });
-  if (matched.length === 0) {
-    backgroundStore.dispatch(inputActions.clearKeys(), sender);
-    return Promise.resolve();
-  } else if (matched.length > 1 ||
-    matched.length === 1 && prefix !== matched[0]) {
-    return Promise.resolve();
-  }
-  let action = settings.keymaps[matched];
-  backgroundStore.dispatch(
-    operationActions.exec(action, sender.tab, settings), sender);
-  backgroundStore.dispatch(inputActions.clearKeys(), sender);
-};
-
-const initializeSettings = () => {
-  backgroundStore.dispatch(settingsActions.load());
-};
-
-initializeSettings();
+backgroundStore.dispatch(settingsActions.load());
---
+++ src/components/background-input.js
@@ -0,0 +1,55 @@
+import * as inputActions from '../actions/input';
+import * as keys from '../shared/keys';
+import * as operationActions from '../actions/operation';
+
+export default class BackgroundInputComponent {
+  constructor(store) {
+    this.store = store;
+    this.keymaps = {};
+    this.prevInputs = [];
+  }
+
+  update(sender) {
+    let state = this.store.getState();
+    this.reloadSettings(state.setting);
+    this.handleKeyInputs(sender, state.input);
+  }
+
+  reloadSettings(setting) {
+    if (!setting.settings.json) {
+      return;
+    }
+    this.keymaps = JSON.parse(setting.settings.json).keymaps;
+  }
+
+  handleKeyInputs(sender, input) {
+    if (JSON.stringify(this.prevInputs) === JSON.stringify(input)) {
+      return;
+    }
+    this.prevInputs = input;
+
+    if (input.keys.length === 0) {
+      return;
+    }
+    if (sender) {
+      return this.handleKeysChanged(sender, input);
+    }
+  }
+
+  handleKeysChanged(sender, input) {
+    let prefix = keys.asKeymapChars(input.keys);
+    let matched = Object.keys(this.keymaps).filter((keyStr) => {
+      return keyStr.startsWith(prefix);
+    });
+    if (matched.length === 0) {
+      this.store.dispatch(inputActions.clearKeys(), sender);
+      return Promise.resolve();
+    } else if (matched.length > 1 ||
+      matched.length === 1 && prefix !== matched[0]) {
+      return Promise.resolve();
+    }
+    let operation = this.keymaps[matched];
+    this.store.dispatch(operationActions.exec(operation, sender.tab), sender);
+    this.store.dispatch(inputActions.clearKeys(), sender);
+  }
+}
