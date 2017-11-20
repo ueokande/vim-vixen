cc539d96bc2d14796279fc2c4c867bb6a3b2df06
Kostyantyn Leschenko <settlerko@gmail.com>
2017-11-20 16:58:39 +0200

    Fix buffers completion when there are some tabs with undefined title.

--- src/background/tabs.js
+++ src/background/tabs.js
@@ -51,7 +51,7 @@ const selectByKeyword = (current, keyword) => {
 const getCompletions = (keyword) => {
   return browser.tabs.query({ currentWindow: true }).then((tabs) => {
     let matched = tabs.filter((t) => {
-      return t.url.includes(keyword) || t.title.includes(keyword);
+      return t.url.includes(keyword) || t.title && t.title.includes(keyword);
     });
     return matched;
   });
