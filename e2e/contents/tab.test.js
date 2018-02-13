b566cfa0acd3679f13e89edbeb35b2d9b4905814
Shin'ya Ueoka <ueokande@i-beam.org>
2018-02-13 20:54:37 +0900

    add press zd test

--- e2e/contents/tab.test.js
+++ e2e/contents/tab.test.js
@@ -7,14 +7,10 @@ const SERVER_URL = "localhost:11111";
 
 describe("tab test", () => {
   let targetWindow;
-  let targetTab;
 
   before(() => {
     return windows.create().then((win) => {
       targetWindow = win;
-      return tabs.create(win.id, SERVER_URL).then((tab) => {
-        targetTab = tab;
-      });
     });
   });
 
@@ -22,13 +18,31 @@ describe("tab test", () => {
     return windows.remove(targetWindow.id);
   });
 
-  it('delete tab', () => {
-    return Promise.resolve().then(() => {
-      return keys.press(targetTab.id, 'd');
-    }).then(() => {
-      return windows.get(targetWindow.id);
-    }).then((after) => {
-      expect(after.tabs).to.have.lengthOf(1);
+  describe('press d', () => {
+    it('deletes tab', () => {
+      return tabs.create(targetWindow.id, SERVER_URL).then((tab) => {
+        return keys.press(tab.id, 'd');
+      }).then(() => {
+        return windows.get(targetWindow.id);
+      }).then((after) => {
+        expect(after.tabs).to.have.lengthOf(1);
+      });
     });
   });
+
+  describe('press zd', () => {
+    it('duplicates tab', () => {
+      let targetTab = 0;
+      return tabs.create(targetWindow.id, SERVER_URL).then((tab) => {
+        targetTab = tab;
+        return keys.press(targetTab.id, 'z');
+      }).then(() => {
+        return keys.press(targetTab.id, 'd');
+      }).then(() => {
+        return windows.get(targetWindow.id);
+      }).then((after) => {
+        expect(after.tabs).to.have.lengthOf(3);
+      });
+    });
+  })
 });
