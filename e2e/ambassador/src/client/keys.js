05faaced137eb3a48780806fded04fd76bd9a84e
Shin'ya Ueoka <ueokande@i-beam.org>
2018-06-28 21:26:48 +0900

    fix for lint

--- e2e/ambassador/src/client/keys.js
+++ e2e/ambassador/src/client/keys.js
@@ -4,28 +4,25 @@ import * as ipc from './ipc';
 const NEUTRAL_MODIFIERS = { shiftKey: false, altKey: false, ctrlKey: false };
 
 const press = (tabId, key, modifiers = NEUTRAL_MODIFIERS) => {
-  return ipc.send(Object.assign({}, modifiers, {
+  return ipc.send({ ...modifiers,
     type: EVENT_KEYPRESS,
     tabId,
-    key,
-  }));
+    key, });
 };
 
 const down = (tabId, key, modifiers = NEUTRAL_MODIFIERS) => {
-  return ipc.send(Object.assign({}, modifiers, {
+  return ipc.send({ modifiers,
     type: EVENT_KEYDOWN,
     tabId,
-    key,
-  }));
+    key, });
 };
 
 
 const up = (tabId, key, modifiers = NEUTRAL_MODIFIERS) => {
-  return ipc.send(Object.assign({}, modifiers, {
+  return ipc.send({ modifiers,
     type: EVENT_KEYUP,
     tabId,
-    key,
-  }));
+    key, });
 };
 
 export { press, down, up };
