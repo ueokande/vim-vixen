b69cc04856fd21d325193d56e212e4dbf07cb762
Shin'ya Ueoka <ueokande@i-beam.org>
2018-07-11 21:01:22 +0900

    Use combineReducers

--- src/content/reducers/index.js
+++ src/content/reducers/index.js
@@ -1,25 +1,10 @@
-import addonReducer from './addon';
-import findReducer from './find';
-import settingReducer from './setting';
-import inputReducer from './input';
-import followControllerReducer from './follow-controller';
+import { combineReducers } from 'redux';
+import addon from './addon';
+import find from './find';
+import setting from './setting';
+import input from './input';
+import followController from './follow-controller';
 
-// Make setting reducer instead of re-use
-const defaultState = {
-  addon: addonReducer(undefined, {}),
-  find: findReducer(undefined, {}),
-  setting: settingReducer(undefined, {}),
-  input: inputReducer(undefined, {}),
-  followController: followControllerReducer(undefined, {}),
-};
-
-export default function reducer(state = defaultState, action = {}) {
-  return {
-    ...state,
-    addon: addonReducer(state.addon, action),
-    find: findReducer(state.find, action),
-    setting: settingReducer(state.setting, action),
-    input: inputReducer(state.input, action),
-    followController: followControllerReducer(state.followController, action),
-  };
-}
+export default combineReducers({
+  addon, find, setting, input, followController,
+});
