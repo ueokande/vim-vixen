b5b6ba0c74853e1f1a2da11ef88d2bb8daeaac37
Shin'ya Ueoka <ueokande@i-beam.org>
2018-07-28 12:36:42 +0900

    Deep-copy objects on MemoryStorage and add tests

--- src/background/infrastructures/memory-storage.js
+++ src/background/infrastructures/memory-storage.js
@@ -2,10 +2,14 @@ const db = {};
 
 export default class MemoryStorage {
   set(name, value) {
-    db[name] = value;
+    let data = JSON.stringify(value);
+    if (typeof data === 'undefined') {
+      throw new Error('value is not serializable');
+    }
+    db[name] = data;
   }
 
   get(name) {
-    return db[name];
+    return JSON.parse(db[name]);
   }
 }
