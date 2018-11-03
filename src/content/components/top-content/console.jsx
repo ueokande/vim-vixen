7ee2acbaa97ea8b497a3a12f8a9d36501b60cc71
Shin'ya Ueoka <ueokande@i-beam.org>
2018-11-04 01:03:27 +0900

    [wip] Completion on console

--- src/content/components/top-content/console.jsx
+++ src/content/components/top-content/console.jsx
@@ -31,10 +31,80 @@ class ConsoleComponent extends Component {
     browser.runtime.onMessage.addListener(this.onMessage.bind(this));
   }
 
-  componentDidUpdate() {
-    switch (this.props.mode) {
-    case 'command':
-    case 'find':
+  onBlur() {
+    return this.context.store.dispatch(consoleActions.hideCommand());
+  }
+
+  onKeyDown(e) {
+    if (e.keyCode === KeyboardEvent.DOM_VK_ESCAPE && e.ctrlKey) {
+      this.context.store.dispatch(consoleActions.hideCommand());
+    }
+    switch (e.keyCode) {
+    case KeyboardEvent.DOM_VK_ESCAPE:
+      return this.context.store.dispatch(consoleActions.hideCommand());
+    case KeyboardEvent.DOM_VK_RETURN:
+      return this.doEnter(e);
+    case KeyboardEvent.DOM_VK_TAB:
+      if (e.shiftKey) {
+        this.context.store.dispatch(consoleActions.completionPrev());
+      } else {
+        this.context.store.dispatch(consoleActions.completionNext());
+      }
+      e.stopPropagation();
+      e.preventDefault();
+      break;
+    case KeyboardEvent.DOM_VK_OPEN_BRACKET:
+      if (e.ctrlKey) {
+        return this.context.store.dispatch(consoleActions.hideCommand());
+      }
+      break;
+    case KeyboardEvent.DOM_VK_M:
+      if (e.ctrlKey) {
+        return this.doEnter(e);
+      }
+      break;
+    case KeyboardEvent.DOM_VK_N:
+      if (e.ctrlKey) {
+        this.context.store.dispatch(consoleActions.completionNext());
+        e.stopPropagation();
+        e.preventDefault();
+      }
+      break;
+    case KeyboardEvent.DOM_VK_P:
+      if (e.ctrlKey) {
+        this.context.store.dispatch(consoleActions.completionPrev());
+        e.stopPropagation();
+        e.preventDefault();
+      }
+      break;
+    }
+  }
+
+  onInput(e) {
+    let text = e.target.value;
+    if (this.props.mode === 'command') {
+      this.context.store.dispatch(consoleActions.setConsoleText(text));
+      this.context.store.dispatch(consoleActions.getCompletions(text));
+    }
+  }
+
+  doEnter(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    let value = e.target.value;
+    if (this.props.mode === 'command') {
+      return this.context.store.dispatch(consoleActions.enterCommand(value));
+    } else if (this.props.mode === 'find') {
+      return this.context.store.dispatch(consoleActions.enterFind(value));
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.mode === '' && this.props.mode === 'command') {
+      this.context.store.dispatch(consoleActions.getCompletions(this.props.consoleText));
+      this.input.focus();
+    } else if (prevProps.mode === '' && this.props.mode === 'find') {
       this.input.focus();
     }
   }
@@ -67,7 +137,12 @@ class ConsoleComponent extends Component {
             <i className={clsName} />
             <input
               ref={(c) => { this.input = c; }}
-              className='vimvixen-console-command-input' />
+              className='vimvixen-console-command-input'
+              onBlur={this.onBlur.bind(this)}
+              onKeyDown={this.onKeyDown.bind(this)}
+              onInput={this.onInput.bind(this)}
+              value={this.props.consoleText}
+            />
           </div>
         </div>
       </div>
@@ -104,6 +179,7 @@ class ConsoleComponent extends Component {
           icon={item.icon}
           caption={item.caption}
           url={item.url}
+          highlight={highlight}
         />);
       }
     }
