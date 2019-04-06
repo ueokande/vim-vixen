b04eecce7fea8b262411a5817924dc730cbe127a
Shin'ya Ueoka <ueokande@i-beam.org>
2019-04-06 21:48:16 +0900

    Replace e2e with lanthan

--- e2e/ambassador/src/background/ipc.js
+++
@@ -1,7 +0,0 @@
-const receiveContentMessage = (func) => {
-  browser.runtime.onMessage.addListener((message) => {
-    return func(message);
-  });
-};
-
-export { receiveContentMessage };
--- e2e/web-server/url.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  PORT: 11111,
-  HOST: '127.0.0.1',
-  CLIENT_URL: 'http://127.0.0.1:11111',
-}
