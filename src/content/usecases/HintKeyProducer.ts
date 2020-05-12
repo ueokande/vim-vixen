696f2bd3b85a56359ef9cdbcc81f47d39bea87e4
MatiasStorm <mat-435@hotmail.com>
2020-05-12 09:33:07 +0200

    Fixed issues with following the first link

--- src/content/usecases/HintKeyProducer.ts
+++ src/content/usecases/HintKeyProducer.ts
@@ -12,6 +12,11 @@ export default class HintKeyProducer {
     this.counter = [];
   }
 
+  setNumberOfHints(nHints: number): void {
+    const charsPrKey : number = Math.ceil(Math.log(nHints) / Math.log(this.charset.length));
+    this.counter = new Array(charsPrKey).fill(0);
+  }
+
   produce(): string {
     this.increment();
 
