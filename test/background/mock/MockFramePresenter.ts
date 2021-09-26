b141c46a2afe31dc7a4bfdf7f59c8871def2de23
Shin'ya Ueoka <ueokande@i-beam.org>
2021-09-26 14:48:01 +0900

    Remove unused file

--- test/background/mock/MockFramePresenter.ts
+++
@@ -1,7 +0,0 @@
-import FramePresenter from "../../../src/background/presenters/FramePresenter";
-
-export default class MockFramePresenter implements FramePresenter {
-  getAllFrameIds(): Promise<number[]> {
-    throw new Error("not implemented");
-  }
-}
